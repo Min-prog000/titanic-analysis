b5bc939800a76d23f624165b1f67f5c615feaeaf
MinPlu <148607818+Min-prog000@users.noreply.github.com>
2025-12-18 21:34:33 +0900

    [feat] (main.py) :sparkles: migrate route handling (#20)

---
+++ src/titanic_analysis/main.py
@@ -0,0 +1,35 @@
+"""Main module for the titanic dataset analysis codes."""
+
+from titanic_analysis.framework.log.log_utils import generate_log_file_path
+from titanic_analysis.framework.log.logger import TitanicLogger
+from titanic_analysis.infrastructure.user.constants import ANALYSIS, TRAINING
+from titanic_analysis.infrastructure.user.parser import generate_parser
+from titanic_analysis.usecase.analysis.service import analyze
+
+
+def main() -> None:
+    """Main entry point of the application."""
+    parser = generate_parser()
+    args = parser.parse_args()
+
+    log_file_path = generate_log_file_path()
+    titanic_logger = TitanicLogger(
+        logger_name=__name__,
+        log_file_name=log_file_path,
+    )
+
+    logger = titanic_logger.logger
+    mode: int = args.mode
+
+    logger.info("Execution mode: %s", mode)
+
+    if mode == ANALYSIS:
+        analyze()
+    elif mode == TRAINING:
+        pass
+    else:
+        logger.warning("Invalid mode inputted.")
+
+
+if __name__ == "__main__":
+    main()

82cf8829a101fc31551ef384e8e2f9108b36d7d8
MinPlu <148607818+Min-prog000@users.noreply.github.com>
2026-02-23 21:24:35 +0900

    [feat] (domain) :sparkles: simplify AI model structure (#85)

--- src/titanic_analysis/domain/model/torch.py
+++ src/titanic_analysis/domain/model/torch.py
@@ -4,17 +4,23 @@ from torch import Tensor, nn
 class NeuralNetwork(nn.Module):
     def __init__(self, feature_size: int) -> None:
         super().__init__()
+        # self.linear_relu_stack = nn.Sequential(
+        #     nn.Linear(in_features=feature_size, out_features=16),
+        #     nn.ReLU(),
+        #     nn.Linear(in_features=16, out_features=32),
+        #     nn.ReLU(),
+        #     nn.Dropout(p=0.1),
+        #     nn.Linear(in_features=32, out_features=16),
+        #     nn.ReLU(),
+        #     nn.Dropout(p=0.1),
+        #     nn.Linear(in_features=16, out_features=8),
+        #     nn.ReLU(),
+        #     nn.Linear(in_features=8, out_features=1),
+        #     nn.Sigmoid(),
+        # )
+
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(in_features=feature_size, out_features=16),
-            nn.ReLU(),
-            nn.Linear(in_features=16, out_features=32),
-            nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(in_features=32, out_features=16),
-            nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(in_features=16, out_features=8),
-            nn.ReLU(),
+            nn.Linear(in_features=feature_size, out_features=8),
             nn.Linear(in_features=8, out_features=1),
             nn.Sigmoid(),
         )

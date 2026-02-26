48a503a23639f778e8468e24783412b899e7b16f
MinPlu <148607818+Min-prog000@users.noreply.github.com>
2026-02-26 22:03:07 +0900

    [feat] (domain) :sparkles: update model to remove fully connected layer and dropout layer (#85)

--- src/titanic_analysis/domain/model/torch.py
+++ src/titanic_analysis/domain/model/torch.py
@@ -5,16 +5,14 @@ class NeuralNetwork(nn.Module):
     def __init__(self, feature_size: int) -> None:
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(in_features=feature_size, out_features=8),
-            nn.ReLU(),
-            nn.Linear(in_features=8, out_features=16),
+            nn.Linear(in_features=feature_size, out_features=16),
             nn.ReLU(),
             nn.Linear(in_features=16, out_features=32),
             nn.ReLU(),
-            nn.Dropout(p=0.1),
+            # nn.Dropout(p=0.3),
             nn.Linear(in_features=32, out_features=16),
             nn.ReLU(),
-            nn.Dropout(p=0.1),
+            # nn.Dropout(p=0.3),
             nn.Linear(in_features=16, out_features=8),
             nn.ReLU(),
             nn.Linear(in_features=8, out_features=1),

0eed1b185e0ff46978df84d6175b67b5f3f95413
MinPlu <148607818+Min-prog000@users.noreply.github.com>
2026-05-09 19:51:11 +0900

    [refactor] (interface, main) :recycle: modularize logger setup and execution‑mode handler initialization (#88)

--- src/titanic_analysis/interface/utils.py
+++ src/titanic_analysis/interface/utils.py
@@ -1,17 +1,87 @@
+"""Utility module for `main.py`"""
+
 from logging import Logger
 
+from titanic_analysis.application.analysis import analyze
+from titanic_analysis.application.prediction import predict
 from titanic_analysis.application.train.sklearn_model import train_sklearn_model
 from titanic_analysis.application.train.torch_model import train_neural_network
-from titanic_analysis.infrastructure.user.constants import TrainMethod
+from titanic_analysis.infrastructure.user.constants import ExecutionMode, TrainMethod
+from titanic_analysis.interface.log.logger import TitanicLogger
+from titanic_analysis.interface.log.utils import generate_log_file_path
+
+__all__ = [
+    "generate_logger",
+    "get_mode_handlers",
+    "log_execution_mode",
+]
 
 
-def train_dispatcher(logger: Logger, train_method: int, execution_mode: int) -> None:
+def generate_logger() -> Logger:
+    """Generate logger and set file handler.
+
+    Returns:
+        Logger: Logger after setting.
+    """
+    log_file_path = generate_log_file_path()
+    titanic_logger = TitanicLogger(
+        logger_name="titanic",
+        log_file_name=log_file_path,
+    )
+
+    return titanic_logger.logger
+
+
+def log_execution_mode(logger: Logger, execution_mode: int) -> None:
+    """Log execution mode.
+
+    Args:
+        logger (Logger): Logger used to inform execution mode.
+        execution_mode (int): Execution mode user directed.
+    """
+    mode_name = ExecutionMode(execution_mode)
+    logger.info("Execution mode: %s", mode_name.name)
+
+
+def get_mode_handlers(
+    logger: Logger,
+    execution_mode: int,
+    train_method: int,
+    model_path: str,
+) -> dict:
+    """Return execution mode handler.
+
+    Args:
+        logger (Logger): Logger.
+        execution_mode (int): Execution mode ID.
+        train_method (int): Train method ID.
+        model_path (str): Model used to predict.
+
+    Returns:
+        dict: Handlers dictionary correlated `ExecutionMode` value and function.
+    """
+    return {
+        ExecutionMode.ANALYSIS.value: lambda: analyze(logger),
+        ExecutionMode.TRAIN.value: lambda: dispatch_training_model(
+            logger,
+            execution_mode,
+            train_method,
+        ),
+        ExecutionMode.PREDICT.value: lambda: predict(logger, model_path),
+    }
+
+
+def dispatch_training_model(
+    logger: Logger,
+    execution_mode: int,
+    train_method: int,
+) -> None:
     """Dispatch training method.
 
     Args:
         logger (Logger): Logger.
-        train_method (int): Training method ID.
         execution_mode (int): Execution mode ID.
+        train_method (int): Training method ID.
     """
     sklearn_methods = {
         TrainMethod.LOGISTIC_REGRESSION.value,
--- src/titanic_analysis/main.py
+++ src/titanic_analysis/main.py
@@ -1,24 +1,17 @@
 """Main module for the titanic dataset analysis codes."""
 
-from titanic_analysis.application.analysis import analyze
-from titanic_analysis.application.prediction import predict
-from titanic_analysis.infrastructure.user.constants import ExecutionMode
 from titanic_analysis.infrastructure.user.parser import generate_parser
-from titanic_analysis.interface.log.logger import TitanicLogger
-from titanic_analysis.interface.log.utils import generate_log_file_path
-from titanic_analysis.interface.utils import train_dispatcher
+from titanic_analysis.interface.utils import (
+    generate_logger,
+    get_mode_handlers,
+    log_execution_mode,
+)
 
 
 def main() -> None:
     """Main entry point of the application."""
-    # logger setting
-    log_file_path = generate_log_file_path()
-    titanic_logger = TitanicLogger(
-        logger_name="titanic",
-        log_file_name=log_file_path,
-    )
-
-    logger = titanic_logger.logger
+    # generate logger
+    logger = generate_logger()
 
     # get arguments
     parser = generate_parser()
@@ -28,20 +21,10 @@ def main() -> None:
     train_method: int = args.train_method
     model_path: str = args.model_path
 
-    mode_name = ExecutionMode(execution_mode)
-    logger.info("Execution mode: %s", mode_name.name)
+    log_execution_mode(logger, execution_mode)
 
-    # strategy choice
-    mode_handlers = {
-        ExecutionMode.ANALYSIS.value: lambda: analyze(logger),
-        ExecutionMode.TRAIN.value: lambda: train_dispatcher(
-            logger,
-            train_method,
-            execution_mode,
-        ),
-        ExecutionMode.PREDICT.value: lambda: predict(logger, model_path),
-    }
-
+    # choose strategy
+    mode_handlers = get_mode_handlers(logger, execution_mode, train_method, model_path)
     handler = mode_handlers.get(execution_mode)
 
     if handler:
